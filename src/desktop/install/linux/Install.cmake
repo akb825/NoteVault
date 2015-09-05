a4bb9261c9dfbad824966adf08a23140bac7851c
Aaron Barany <akb825@gmail.com>
2015-09-05 15:59:28 -0700

    Path fix for Linux installation

--- src/desktop/install/linux/Install.cmake
+++ src/desktop/install/linux/Install.cmake
@@ -1,6 +1,6 @@
 install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/../../assets/linux/icons/
 	DESTINATION share/icons/hicolor)
-install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/install/Linux/notevault.desktop
+install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/install/linux/notevault.desktop
 	DESTINATION share/applications)
-install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/install/Linux/secure-note.xml
+install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/install/linux/secure-note.xml
 	DESTINATION share/mime/packages)
