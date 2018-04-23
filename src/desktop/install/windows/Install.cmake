461af9d062eb3bd3164a422e322cee0030bcf2b0
Aaron Barany <akb825@gmail.com>
2018-04-22 19:50:36 -0700

    Update WIndows installation.
    Finding the proper DLLs is now more robust. Use find_file() to find the proper files, which
    should work better for SSL due to the option to install DLLs in the System directory. Also use
    CMake's ability to install the runtime DLL rather than manually specifying the DLL name.
    (Still need to hard-code runtime DLL for SSL since it's pre-built with a different version of
    Visual Studio)

--- src/desktop/install/windows/Install.cmake
+++ src/desktop/install/windows/Install.cmake
@@ -1,15 +1,35 @@
 # Install dependent DLLs
-get_filename_component(sslBase ${OPENSSL_INCLUDE_DIR} DIRECTORY)
-list(GET Qt5Widgets_INCLUDE_DIRS 0 qtInclude)
-get_filename_component(qtBase ${qtInclude} DIRECTORY)
 
-install(FILES ${qtBase}/bin/Qt5Core.dll DESTINATION bin)
-install(FILES ${qtBase}/bin/Qt5Gui.dll DESTINATION bin)
-install(FILES ${qtBase}/bin/Qt5Widgets.dll DESTINATION bin)
-install(FILES ${qtBase}/bin/Qt5Svg.dll DESTINATION bin)
-install(FILES ${sslBase}/bin/libeay32.dll DESTINATION bin)
-install(FILES C:/Windows/System32/msvcp120.dll DESTINATION bin)
-install(FILES C:/Windows/System32/msvcr120.dll DESTINATION bin)
+# Qt
+find_file(Qt5Core Qt5Core.dll)
+find_file(Qt5Gui Qt5Gui.dll)
+find_file(Qt5Widgets Qt5Widgets.dll)
+find_file(Qt5Svg Qt5Svg.dll)
+install(FILES ${Qt5Core} ${Qt5Gui} ${Qt5Widgets} ${Qt5Svg} DESTINATION bin)
+
+# Also the platform.
+get_filename_component(qt5Base ${Qt5Core} DIRECTORY)
+get_filename_component(qt5Base ${qt5Base} DIRECTORY)
+find_file(qwindows qwindows.dll PATHS ${qt5Base}/plugins/platforms)
+install(FILES ${qwindows} DESTINATION bin/platforms)
+
+# OpenSSL
+string(REGEX REPLACE "^([0-9]+)\\.([0-9]+)\\..*" "\\1_\\2" version ${OPENSSL_VERSION})
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	set(platformSuffix "-x64")
+else()
+	set(platformSuffix)
+endif()
+set(cryptoName libcrypto-${version}${platformSuffix}.dll)
+find_file(libcrypto ${cryptoName})
+install(FILES ${libcrypto} DESTINATION bin)
+
+# OpenSSL currently also uses VS2013 redistributable.
+# Make it optional in case this breaks in the future.
+find_file(msvcr120 msvcr120.dll)
+if (msvcr120)
+	install(FILES ${msvcr120} DESTINATION bin)
+endif()
 
 set(CPACK_GENERATOR WIX)
 set(CPACK_WIX_UPGRADE_GUID 5F45FB31-9A30-4501-9E65-6AB9E8FDAF34)
